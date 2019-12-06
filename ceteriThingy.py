e6cc3fd0c86163e28082e768eb22f5bc3af227fb
Bliffy <56902179+BliffyB@users.noreply.github.com>
2019-12-06 13:45:24 -0600

    start

---
+++ ceteriThingy.py
@@ -0,0 +1,38 @@
+import random
+import os,time
+
+
+
+
+
+
+
+
+
+"""main"""
+os.system('cls')
+filenames = ["new1"]
+frames = []
+for name in filenames:
+    with open(name,"r",encoding="utf8") as f:
+        frames.append (f.readlines())
+
+for frame in frames:
+    print("".join(frame))
+    time.sleep(1)
+print("\n")
+name = input('Hello there what is your name?\n')
+if (name == "Mark"): name = random.choice( ['yarini', 'yarpini', 'i cant say your user name it too hard :( '] )   
+print("hi "+ name) 
+guildname = random.choice( ['A dead guild', 'ceteri', 'shiteri'] )
+print("whalecome to the super awesome "+ guildname + " database \n ")
+print ("What you like to acess? :")
+print ("1. Menu 1")
+print ("2. Menu 2")
+print ("3. Menu 3")
+print ("\n0. Quit")
+choice = input(" >>  ")
+print ("oh wait we have no data here. Thanks bliffy you lazy poop")
+
+
+
