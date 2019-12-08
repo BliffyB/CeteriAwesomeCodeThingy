dc6ece3233e990320e808fea07861b4a4511d368
Bliffy <56902179+BliffyB@users.noreply.github.com>
2019-12-07 20:28:22 -0600

    Update ceteriThingy.py

--- ceteriThingy.py
+++ ceteriThingy.py
@@ -1,13 +1,34 @@
 import random
 import os,time
+# Main menu
+def main_menu():
+    print ("1. Ceteri's collage resheach papers about (Are traps gay?)")
+    print ("2. That ass ;D")
+    print ("\n0. Quit")
+    choice = input(" >>  ")
 
+    exec_menu(choice)
+    return
 
-def menu(choice):
-    if (choice == 0): print('Saturday0')
-    if (choice == 1): print('Saturday0')           
-    if (choice == 2): print('Saturday0')         
-    if (choice == 3): datAss
-    if (choice == 4): tarpsAre
+# Execute menu
+def exec_menu(choice):
+    ch = choice.lower()
+    if ch == '':
+        menu_actions['main_menu']()
+    else:
+        try:
+            menu_actions[ch]()
+        except KeyError:
+            print ("Invalid selection, please try again.\n")
+            menu_actions['main_menu']()
+    return
+# Back to main menu
+def back():
+    menu_actions['main_menu']()
+ 
+# Exit program
+def exit():
+    sys.exit()
 
 def trapsAre():
     print ("whalcume to are grat skdjaklajawkljafaf")
@@ -16,9 +37,19 @@ def trapsAre():
 
 def datAss():
     frames.append (f.readlines())
-
-
-
+    
+# =======================
+#    MENUS DEFINITIONS
+# =======================
+ 
+# Menu definition
+menu_actions = {
+    'main_menu': main_menu,
+    '1': trapsAre,
+    '2': datAss,
+    '9': back,
+    '0': exit,
+}
 
 os.system('cls')
 filenames = ["new1"]
@@ -37,12 +68,7 @@ print("hi "+ name)
 guildname = random.choice( ['A dead guild', 'ceteri', 'shiteri'] )
 print("whalecome to the super awesome "+ guildname + " database \n ")
 print ("What you like to acess? :")
-print ("1. Menu 1")
-print ("2. Menu 2")
-print ("3. That ass ;D")
-print ("4. Ceteri's collage resheach papers about (Are traps gay?)")
-print ("\n0. Quit")
-choice = input(" >>  ")
-print ("oh wait we have no data here. Thanks bliffy you lazy poop")
-menu(choice)
+main_menu()
+# print ("oh wait we have no data here. Thanks bliffy you lazy poop")
+
 
